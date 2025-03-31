d439c0208b7da865900cdf49a61953bc5d038dc4
Abhay Thakur <168988181+abhayt583@users.noreply.github.com>
2025-03-31 22:02:36 +0530

    Time: 0 ms (100%), Space: 18.2 MB (42.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,26 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low=0
-        high=len(nums)-1
-        while low <= high:
-            mid = (low + high) // 2  # Calculate mid pointer
-
-            # Step 1: Check if mid points to the target
-            if nums[mid] == target:
-                return mid  # Return index of the target
-            
-            # Step 2: Identify the sorted half
-            if nums[low] <= nums[mid]:  # Left half is sorted
-                # Check if target is in the sorted left half
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1  # Eliminate the right half
-                else:
-                    low = mid + 1  # Eliminate the left half
-            else:  # Right half is sorted
-                # Check if target is in the sorted right half
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1  # Eliminate the left half
-                else:
-                    high = mid - 1  # Eliminate the right half
-        return -1
+       
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        pivot = l 
         
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            # Adjust mid index to account for rotation
+            realMid = (mid + pivot) % len(nums)
+            if nums[realMid] == target:
+                return realMid  
+            elif nums[realMid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+     
+        return -1
