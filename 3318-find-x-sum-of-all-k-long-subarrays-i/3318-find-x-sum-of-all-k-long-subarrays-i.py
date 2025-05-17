ca64bf3efe4783bd336452710723562afde0f0e0
Abhay Thakur <168988181+abhayt583@users.noreply.github.com>
2025-05-17 12:25:17 +0530

    Time: 53 ms (5.11%), Space: 18 MB (13.05%) - LeetHub

--- 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,59 +1,47 @@
-from typing import List
+
 
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
-        n = len(nums)
-        
-        
-        for i in range(n - k + 1):
-         
-            freq = {}  
-            j = i
-            while j < i + k:
+
+        for i in range(len(nums) - k + 1):
+            freq = {}
+
+            # Count occurrences manually for the subarray nums[i:i+k]
+            for j in range(i, i + k):
                 num = nums[j]
-                
                 if num in freq:
-                    freq[num] = freq[num] + 1
+                    freq[num] += 1
                 else:
                     freq[num] = 1
-                j += 1
-
-           
+            
+            # Convert dictionary to list of [count, value] pairs
             pairs = []
             for key in freq:
-                pair = [freq[key], key]  
-                pairs.append(pair)
+                pairs.append([freq[key], key])  # [count, value]
 
-            m = len(pairs)
-           
-            for a in range(m):
-                max_index = a
-                for b in range(a + 1, m):
-                    
-                    if pairs[b][0] > pairs[max_index][0]:
-                        max_index = b
-                  
-                    elif pairs[b][0] == pairs[max_index][0]:
-                        if pairs[b][1] > pairs[max_index][1]:
-                            max_index = b
+            # Manually sorting by frequency and then by value
+            for a in range(len(pairs)):
+                for b in range(a + 1, len(pairs)):
+                    if pairs[b][0] > pairs[a][0]:  # Sort by count descending
+                        pairs[a], pairs[b] = pairs[b], pairs[a]
+                    elif pairs[b][0] == pairs[a][0]:  # If count is the same, sort by value descending
+                        if pairs[b][1] > pairs[a][1]:
+                            pairs[a], pairs[b] = pairs[b], pairs[a]
 
-                if max_index != a:
-                    temp = pairs[a]
-                    pairs[a] = pairs[max_index]
-                    pairs[max_index] = temp
-
-
+            # Compute sum of top x elements
             sum_top_x = 0
             count_taken = 0
-            idx = 0
-            while idx < m and count_taken < x:
-                sum_top_x = sum_top_x + pairs[idx][0] * pairs[idx][1]
-                count_taken = count_taken + 1
-                idx = idx + 1
-
+            for pair in pairs:
+                if count_taken < x:
+                    sum_top_x += pair[0] * pair[1]
+                    count_taken += 1
+                else:
+                    break
 
             res.append(sum_top_x)
-        
+
         return res
+
+
 
