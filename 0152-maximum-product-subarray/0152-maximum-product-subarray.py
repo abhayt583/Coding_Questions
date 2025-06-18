597189eca5033e7bdbbfd554acc95f683451728e
Abhay Thakur <168988181+abhayt583@users.noreply.github.com>
2025-06-18 23:58:10 +0530

    Time: 11 ms (51.57%), Space: 18.2 MB (80.59%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+        n = len(nums)
+
+        for i in range(1, n):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(result, max_so_far)
+
+        return result
